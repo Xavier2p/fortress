c54a545756abfb7ffd1006fc8f11b511ce2d306e
Xavier2p <git@Xavier2p.fr>
2025-09-25 12:57:49 +0200

    feat: add new error handler

---
+++ src/helpers/errors.rs
@@ -0,0 +1,86 @@
+use std::fmt::Debug;
+use std::io;
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub enum FortressError {
+    VaultAlreadyExists,
+    VaultNotFound,
+    InvalidVaultPath,
+    InvalidCommand,
+    DecryptionFailed,
+    EncryptionFailed,
+    IoError(io::Error),
+    SerializationError(serde_json::Error),
+    InvalidMasterPassword,
+    CorruptedVault,
+}
+
+impl std::error::Error for FortressError {}
+
+impl std::fmt::Display for FortressError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            FortressError::CorruptedVault => {
+                write!(
+                    f,
+                    "CorruptedVault: The selected vault is corrupted or has been tampered."
+                )
+            }
+            FortressError::InvalidMasterPassword => {
+                write!(
+                    f,
+                    "InvalidMasterPassword: The provided master password is incorrect."
+                )
+            }
+            FortressError::VaultAlreadyExists => {
+                write!(
+                    f,
+                    "VaultAlreadyExists: The vault already exists at the specified path."
+                )
+            }
+            FortressError::VaultNotFound => {
+                write!(
+                    f,
+                    "VaultNotFound: The vault file was not found at the specified path."
+                )
+            }
+            FortressError::InvalidVaultPath => {
+                write!(f, "InvalidVaultPath: The specified vault path is invalid.")
+            }
+            FortressError::InvalidCommand => {
+                write!(f, "InvalidCommand: The provided command is invalid.")
+            }
+            FortressError::DecryptionFailed => write!(
+                f,
+                "DecryptionFailed: Failed to decrypt the vault. Possible reasons include an incorrect master password or corrupted data."
+            ),
+            FortressError::EncryptionFailed => {
+                write!(f, "EncryptionFailed: Failed to encrypt the vault data.")
+            }
+            FortressError::IoError(e) => write!(f, "IoError: {}", e),
+            FortressError::SerializationError(e) => write!(f, "SerializationError: {}", e),
+        }
+    }
+}
+
+impl From<io::Error> for FortressError {
+    fn from(error: io::Error) -> Self {
+        if error.kind() == io::ErrorKind::NotFound {
+            FortressError::VaultNotFound
+        } else {
+            FortressError::IoError(error)
+        }
+    }
+}
+
+impl From<serde_json::Error> for FortressError {
+    fn from(error: serde_json::Error) -> Self {
+        FortressError::SerializationError(error)
+    }
+}
+
+pub fn raise(error: FortressError) {
+    eprintln!("Error: {}", error);
+    std::process::exit(1);
+}
--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -1,2 +1,2 @@
 pub mod cli;
-mod errors;
+pub mod errors;
