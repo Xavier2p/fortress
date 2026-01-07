8e78225206a0fa4b1f4faf58e5d44599c36dc340
Xavier2p <git@Xavier2p.fr>
2026-01-07 13:31:26 +0100

    feat(flake): build rust package directly with nix

--- flake.nix
+++ flake.nix
@@ -1,35 +1,47 @@
 {
   description = "A simple password manager written in Rust";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {
-          system = system;
-          config.allowUnfree = true;
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in {
+    packages.${system}.default = let
+      manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+    in
+      pkgs.rustPlatform.buildRustPackage rec {
+        pname = manifest.name;
+        version = manifest.version;
+        cargoLock.lockFile = ./Cargo.lock;
+        src = pkgs.lib.cleanSource ./.;
+
+        meta = {
+          description = manifest.description;
+          homepage = manifest.repository;
+          maintainers = manifest.authors;
+          mainProgram = "frtrs";
         };
-      in {
-        inherit pkgs;
-        devShells.default = pkgs.mkShell {
-          name = "rust";
-          buildInputs = with pkgs; [
-            bacon
-            cargo
-            cargo-audit
-            gcc
-            rustup
-            wrkflw
-          ];
-        };
-      }
-    );
+      };
+
+    devShells.${system}.default = pkgs.mkShell {
+      name = "rust";
+      buildInputs = with pkgs; [
+        bacon
+        cargo
+        cargo-audit
+        clippy
+        gcc
+        rustc
+        rustfmt
+        wrkflw
+      ];
+    };
+  };
 }
